152f9bf756d90970ef3bb3f2bc685e0abdf0dff8
JERRYFROMKENYA <jerryochieng178@gmail.com>
2022-07-14 12:58:43 +0300

    yaay! more personal projects!!!!

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Button } from '../Button/Button'
+import './herosection.css'
+
+
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+        <video src='./videos/video-1.mp4' autoPlay loop muted />
+        <h1>Welcome</h1>
+        <h2>to</h2>
+        <h3> jerrfromkenya.space</h3>
+        <p></p>
+        <div className="hero-btns">
+            <Button className='btns' link='/portfolio' buttonStyle='btn--outline'
+            buttonSize='btn--large'>CHECK MY CURRICULUM VITAE OUT.</Button>
+             <Button className='btns' buttonStyle='btn--primary'
+            buttonSize='btn--large' link='/contact'>CONTACT JERR <i className="fa-solid fa-message-code"></i>  </Button>
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HeroSection from '../../components/HeroSection/HeroSection'
+import Cards from '../../components/Cards/Cards'
+
+export default function Home() {
+  return (
+    <>
+    <HeroSection />
+    {/*Home section*/}
+    <Cards />
+
+
+    </>
+  )
+}

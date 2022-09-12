8343a1f6f35f0ee59b400d94cc65652770afbd16
Jerry Ochieng <jerryochieng178@gmail.com>
2022-09-12 09:46:15 +0300

    Update Single.jsx

--- src/pages/Single/Single.jsx
+++ src/pages/Single/Single.jsx
@@ -1,4 +1,4 @@
-//import Sidebar from "../../components/sidebar/Sidebar"
+import Sidebar from "../../components/Sidebar/Sidebar"
 import SinglePost from "../../components/singlePost/SinglePost"
 import "./single.css"
 
